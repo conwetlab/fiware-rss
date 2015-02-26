06d195d1f1031527e8270ef46381e486d1800250
fdelavega <fdelavega@conwet.com>
2015-02-26 16:20:42 +0100

    Use createOrUpdate instead of update for creating new user expenditure limits

--- rss-expendLimit/el-server/src/main/java/es/tid/fiware/rss/expenditureLimit/server/service/ExpenditureLimitManager.java
+++ rss-expendLimit/el-server/src/main/java/es/tid/fiware/rss/expenditureLimit/server/service/ExpenditureLimitManager.java
@@ -2,6 +2,8 @@
  * Revenue Settlement and Sharing System GE
  * Copyright (C) 2011-2014, Javier Lucio - lucio@tid.es
  * Telefonica Investigacion y Desarrollo, S.A.
+ *
+ * Copyright (C) 2015, CoNWeT Lab., Universidad Politécnica de Madrid
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -401,7 +403,7 @@ public class ExpenditureLimitManager {
     private void insertNewLimits(List<DbeExpendLimit> newLimits) {
         ExpenditureLimitManager.logger.debug("Into insertNewLimits method. Limits size: " + newLimits.size());
         for (DbeExpendLimit limit : newLimits) {
-            expLimitDao.update(limit);
+            expLimitDao.createOrUpdate(limit);
         }
     }
 
