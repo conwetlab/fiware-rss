76a221d39e7b64abed08c2727c2c91f7226ab034
Javier Ortiz Sancho <jortiz@conwet.com>
2015-09-28 19:18:05 +0200

    Add new tests to SettlementManagerTest.java
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- fiware-rss/src/test/java/es/tid/fiware/rss/service/SettlementManagerTest.java
+++ fiware-rss/src/test/java/es/tid/fiware/rss/service/SettlementManagerTest.java
@@ -21,22 +21,27 @@
 
 package es.tid.fiware.rss.service;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import es.tid.fiware.rss.dao.DbeTransactionDao;
 import es.tid.fiware.rss.exception.RSSException;
+import es.tid.fiware.rss.model.Aggregator;
 import es.tid.fiware.rss.model.DbeTransaction;
+import es.tid.fiware.rss.model.RSSModel;
+import es.tid.fiware.rss.model.RSSProvider;
+import es.tid.fiware.rss.settlement.ProductSettlementTask;
 import es.tid.fiware.rss.settlement.SettlementTaskFactory;
 import es.tid.fiware.rss.settlement.ThreadPoolManager;
-import java.io.File;
+import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.Properties;
-import static org.junit.Assert.fail;
+import java.util.concurrent.ExecutorService;
 import org.mockito.InjectMocks;
+import static org.mockito.Matchers.any;
 import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
@@ -56,12 +61,137 @@ public class SettlementManagerTest {
     }
 
     @Test
-    //TODO:
     public void runSettlementTest() throws RSSException {
-
         String aggregatorId = "aggregator@mail.com";
         String providerId = "provider@mail.com";
         String productClass = "productClass";
+
+        Aggregator aggregator = new Aggregator();
+        aggregator.setAggregatorId(aggregatorId);
+        aggregator.setAggregatorName(aggregatorId);
+
+        List <RSSProvider> providers = new LinkedList<>();
+        RSSProvider rSSProvider = new RSSProvider();
+        rSSProvider.setAggregatorId(aggregatorId);
+        rSSProvider.setProviderId(providerId);
+        rSSProvider.setProviderName(providerId);
+        providers.add(rSSProvider);
+
+        List <RSSModel> models = new LinkedList<>();
+        RSSModel model = new RSSModel();
+        model.setAggregatorId(aggregatorId);
+        model.setAggregatorShare(BigDecimal.ZERO);
+        model.setAlgorithmType(aggregatorId);
+        model.setOwnerProviderId(providerId);
+        model.setOwnerValue(BigDecimal.ZERO);
+        model.setProductClass(productClass);
+        model.setStakeholders(null);
+        models.add(model);
+
+        when(modelsManager.existModel(aggregatorId, providerId, productClass)).thenReturn(Boolean.TRUE);
+        when(aggregatorManager.getAggregator(aggregatorId)).thenReturn(aggregator);
+        when(providerManager.getAPIProviders(aggregatorId)).thenReturn(providers);
+        when(providerManager.getProvider(aggregatorId, providerId)).thenReturn(rSSProvider);
+        when(modelsManager.getRssModels(aggregatorId, providerId, productClass)).thenReturn(models);
+
+        toTest.runSettlement(aggregatorId, providerId, productClass);
+    }
+
+    @Test
+    public void runSettlement2Test() throws RSSException {
+        String aggregatorId = "aggregator@mail.com";
+        String providerId = "provider@mail.com";
+        String productClass = "productClass";
+
+        Aggregator aggregator = new Aggregator();
+        aggregator.setAggregatorId(aggregatorId);
+        aggregator.setAggregatorName(aggregatorId);
+
+        List <RSSProvider> providers = new LinkedList<>();
+        RSSProvider rSSProvider = new RSSProvider();
+        rSSProvider.setAggregatorId(aggregatorId);
+        rSSProvider.setProviderId(providerId);
+        rSSProvider.setProviderName(providerId);
+        providers.add(rSSProvider);
+
+        List <RSSModel> models = new LinkedList<>();
+        RSSModel model = new RSSModel();
+        model.setAggregatorId(aggregatorId);
+        model.setAggregatorShare(BigDecimal.ZERO);
+        model.setAlgorithmType(aggregatorId);
+        model.setOwnerProviderId(providerId);
+        model.setOwnerValue(BigDecimal.ZERO);
+        model.setProductClass(productClass);
+        model.setStakeholders(null);
+        models.add(model);
+
+        List <DbeTransaction> transactions = new LinkedList<>();
+        DbeTransaction transaction = new DbeTransaction();
+        transactions.add(transaction);
+
+        ProductSettlementTask settlementTask = new ProductSettlementTask();
+        ExecutorService executorService = mock(ExecutorService.class);
+
+        when(modelsManager.existModel(aggregatorId, providerId, productClass)).thenReturn(true);
+        when(aggregatorManager.getAggregator(aggregatorId)).thenReturn(aggregator);
+        when(providerManager.getAPIProviders(aggregatorId)).thenReturn(providers);
+        when(providerManager.getProvider(aggregatorId, providerId)).thenReturn(rSSProvider);
+        when(modelsManager.getRssModels(aggregatorId, providerId, productClass)).thenReturn(models);
+        when(transactionDao.getTransactions(aggregatorId, providerId, productClass)).thenReturn(transactions);
+        when(taskFactory.getSettlementTask(any(RSSModel.class), any(List.class))).thenReturn(settlementTask);
+        when(poolManager.getExecutorService()).thenReturn(executorService);
+
+        toTest.runSettlement(aggregatorId, providerId, productClass);
+    }
+
+    @Test
+    public void runSettlementNullArgumentsTest() throws RSSException {
+        String aggregatorId = null;
+        String providerId = null;
+        String productClass = null;
+
+
+        List <Aggregator> aggregators = new LinkedList();
+        Aggregator aggregator = new Aggregator();
+        aggregator.setAggregatorId(aggregatorId);
+        aggregator.setAggregatorName(aggregatorId);
+        aggregators.add(aggregator);
+
+        List <RSSProvider> providers = new LinkedList<>();
+        RSSProvider rSSProvider = new RSSProvider();
+        rSSProvider.setAggregatorId(aggregatorId);
+        rSSProvider.setProviderId(providerId);
+        rSSProvider.setProviderName(providerId);
+        providers.add(rSSProvider);
+
+        List <RSSModel> models = new LinkedList<>();
+        RSSModel model = new RSSModel();
+        model.setAggregatorId(aggregatorId);
+        model.setAggregatorShare(BigDecimal.ZERO);
+        model.setAlgorithmType(aggregatorId);
+        model.setOwnerProviderId(providerId);
+        model.setOwnerValue(BigDecimal.ZERO);
+        model.setProductClass(productClass);
+        model.setStakeholders(null);
+        models.add(model);
+
+        when(modelsManager.existModel(aggregatorId, providerId, productClass)).thenReturn(Boolean.TRUE);
+        when(aggregatorManager.getAPIAggregators()).thenReturn(aggregators);
+        when(providerManager.getAPIProviders(aggregatorId)).thenReturn(providers);
+        when(providerManager.getProvider(aggregatorId, providerId)).thenReturn(rSSProvider);
+        when(modelsManager.getRssModels(aggregatorId, providerId, productClass)).thenReturn(models);
+
+        toTest.runSettlement(aggregatorId, providerId, productClass);
+    }
+
+    @Test
+    (expected = RSSException.class)
+    public void runSettlementRSSExceptionTest() throws RSSException {
+        String aggregatorId = "aggregator@mail.com";
+        String providerId = "provider@mail.com";
+        String productClass = "productClass";
+
+        when(modelsManager.existModel(aggregatorId, providerId, productClass)).thenReturn(false);
 
         toTest.runSettlement(aggregatorId, providerId, productClass);
     }
