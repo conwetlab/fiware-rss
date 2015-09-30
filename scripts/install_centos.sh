10a197979c7953117c096ea8031ed37bf5ef5b8a
fdelavega <fdelavega@conwet.com>
2015-09-30 18:12:04 +0200

    Install oficial MySQL from install_centos.sh

--- scripts/install_centos.sh
+++ scripts/install_centos.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 sudo yum update
-sudo yum install -y openjdk-7-jdk tomcat7 mysql-client mysql-server
+sudo yum install -y java-1.7.0-openjdk-devel tomcat
+
+sudo rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+sudo yum -y install mysql-community-server
 
 # Install maven if needed
 if [ -d "$INSPWD/fiware-rss" ]; then
     sudo yum install -y  install maven
 fi
 
-sudo service mysqld restart
+sudo /usr/bin/systemctl enable mysqld
+sudo /usr/bin/systemctl start mysqld
+
 sudo /usr/bin/mysql_secure_installation
